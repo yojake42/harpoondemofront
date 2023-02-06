1dd6fc5d8c95b851450c8ce2318c0877ef200b85
Mat Jackson <matjack1121@gmail.com>
2023-02-06 08:19:06 -0700

    Add files via upload

---
+++ harpoonDemoFront/client/src/components/create.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Create() {
+  const [form, setForm] = useState({
+    name: "",
+    position: "",
+    level: "",
+  });
+  const navigate = useNavigate();
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  // This function will handle the submission.
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    // When a post request is sent to the create url, we'll add a new record to the database.
+    const newPerson = { ...form };
+
+    await fetch("http://localhost:5000/record/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPerson),
+    })
+    .catch(error => {
+      window.alert(error);
+      return;
+    });
+
+    setForm({ name: "", position: "", level: "" });
+    navigate("/");
+  }
+
+  // This following section will display the form that takes the input from the user.
+  return (
+    <div>
+      <h3>Create New Person</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="position">Relation</label>
+          <input
+            type="text"
+            className="form-control"
+            id="position"
+            value={form.position}
+            onChange={(e) => updateForm({ position: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create person"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
