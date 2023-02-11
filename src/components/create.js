8f9a30a3aa09daf7fbe5d954f23423f9d82773f1
Mat Jackson <43732109+maja5627@users.noreply.github.com>
2023-02-10 17:14:52 -0700

    try no proxy pass

--- src/components/create.js
+++ src/components/create.js
@@ -23,7 +23,7 @@ export default function Create() {
 
     // When a post request is sent to the create url, we'll add a new record to the database.
     const newPerson = { ...form };
-    await fetch( `/mybackendserver/record/add`, {
+    await fetch( `http://srv-63e57896d6634700175dd5cb-internal.harpoon-78739-26567.svc.cluster.local:5000/record/add`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
